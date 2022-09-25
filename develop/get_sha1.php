6cac086f2ccb03d68aeb4aab31c7842da11b2a93
jsavargas <jsavargas@gmail.com>
2022-09-24 23:42:28 -0300

    correjido json

--- develop/get_sha1.php
+++ develop/get_sha1.php
@@ -3,27 +3,15 @@
 
 $version = isset($argv)? $argv[1]: '6.11';
 
-$file = '/mnt/user/jonathan/git/unRAID/plugins/unRAID-NerdPack/packages/6.11/utf8proc-2.7.0-x86_64-1.txz';
-
-$sha=file_check_sha1($file,'904236d09f2f6912e2d7d44dfd23354ee9a411ee');
-
-
 
 $array = array();
 
-$files = glob("/mnt/user/jonathan/git/unRAID/plugins/unRAID-NerdPack/packages/$version/*.{txz,tgz}", GLOB_BRACE);
+$files = glob(__DIR__."/../packages/$version/*.{txz,tgz}", GLOB_BRACE);
 foreach($files as $txz){
     //echo $txz, "\n";
-   
 
     $array[] = file_check_sha1($txz);
 }
-
-
-
-
-
-
 
 // Compare the github sha1 value of a file
 function file_check_sha1($file) {
@@ -47,7 +35,7 @@ function file_check_sha1($file) {
     $detalle['path'] = "packages/$version/".basename($file);
     $detalle['sha'] = $sha1_file;
     $detalle['size'] = $size;
-    $detalle['download_url'] = "https://raw.githubusercontent.com/jsavargas/unRAID-NerdPack/update/packages/$version/".basename($file);
+    $detalle['download_url'] = "https://raw.githubusercontent.com/UnRAIDES/unRAID-NerdPack/master/packages/$version/".basename($file);
     $detalle['type'] = 'file';
 
     //print_r($detalle);
